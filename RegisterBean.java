19731294e55ebca0a1e2a30b0f50a700d602eeb5
Denes Kosztyuk <48892383+deneskosztyuk@users.noreply.github.com>
2024-11-04 22:38:20 +0000

    Update RegisterBean.java

--- RegisterBean.java
+++ RegisterBean.java
@@ -3,29 +3,21 @@ package com.example.adastra_tomcat;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 
-
-
 @Named
 @RequestScoped
 public class RegisterBean {
     private String username;
     private String email;
     private String password;
-
-    // Getters and setters
-
     public String register() {
-        // Implement your registration logic here
-        // You'll need to create a UserDAO class to handle database operations
-
         UserDAO userDAO = new UserDAO();
         User user = new User(username, password, email);
         boolean registered = userDAO.registerUser(user);
 
         if (registered) {
-            return "success"; // Redirect to a success page
+            return "success"; 
         } else {
-            return "failure"; // Redirect to a failure page
+            return "failure";
         }
     }
 }
